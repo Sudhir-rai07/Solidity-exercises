e8f1f53f064b36d099182637c179bd698051bffa
Sudhir-rai07 <sudhirrai128@gmail.com>
2025-01-10 15:46:32 +0530

    Loops

--- contracts/07Loops.sol
+++ contracts/07Loops.sol
@@ -2,44 +2,44 @@
 
 pragma solidity ^0.8.0;
 
-contract ExpenceTeacker {
-    // Create a struct of Expence
+contract ExpenseTeacker {
+    // Create a struct of Expense
     // Fields : user, description, amount
-    // Create a function to get total expence
+    // Create a function to get total expense
 
-    struct Expence {
+    struct Expense {
         address user;
         string description;
         uint amount;
     }
 
-    Expence[] public expences;
+    Expense[] public expenses;
 
     constructor() {
-        expences.push(Expence(msg.sender, "Movie", 2));
-        expences.push(Expence(msg.sender, "Cloths", 28));
-        expences.push(Expence(msg.sender, "Books", 12));
-        expences.push(Expence(msg.sender, "Party", 222));
+        expenses.push(Expense(msg.sender, "Movie", 2));
+        expenses.push(Expense(msg.sender, "Cloths", 28));
+        expenses.push(Expense(msg.sender, "Books", 12));
+        expenses.push(Expense(msg.sender, "Party", 222));
     }
 
-    // fn to add a new expence
-    function addExpence(string memory _description, uint _amount) public {
-        Expence memory newExpence = Expence({
+    // fn to add a new expense
+    function addExpense(string memory _description, uint _amount) public {
+        Expense memory newExpense = Expense({
             user : msg.sender,
             description : _description,
             amount: _amount
         });
 
-        // Push the expence to expences
-        expences.push(Expence(msg.sender, newExpence.description, newExpence.amount));
+        // Push the expense to expenses
+        expenses.push(Expense(msg.sender, newExpense.description, newExpense.amount));
     }
 
-    // fn to get total expence by user address
+    // fn to get total expense by user address
     function totalEx(address _user) public view returns(uint){
         uint total;
-        for(uint i = 0; i < expences.length; i++){
-            if(expences[i].user == _user){
-                total += expences[i].amount;
+        for(uint i = 0; i < expenses.length; i++){
+            if(expenses[i].user == _user){
+                total += expenses[i].amount;
             }
         }
 
